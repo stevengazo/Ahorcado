7b1be0e19c580360e2cbbea66bfacf127f8ac207
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2021-12-31 17:40:47 -0600

    cambio entre interfaces funcionales

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,17 +1,46 @@
 const div1 = document.getElementById("Part1");
 const inputWorld = document.getElementById("world");
 
+const errorMessage = document.getElementById("message");
 const input1 = document.getElementById("prueba1");
 const input2 = document.getElementById("prueba2");
 const input3 = document.getElementById("prueba3");
 
-function sample(){
-    alert(`sample`);
-    div1.style.display= "none";
-}        
+
+
+
+function initPlay(){
+    const IsValid = validateValues();
+    if(IsValid){
+        errorMessage.innerText= "";
+        alert("cambio");
+        div1.style.display= "none";
+    }
+    else{
+        errorMessage.innerText= "¡No introdujo los datos necesarios!";
+    }
+}
+   
 const cleanInputs = ()=>{
     inputWorld.value="";
     input1.value= "";
     input2.value= "";
     input3.value= "";
 };
+
+
+const validateValues = function(){
+    let bandInputWorld = (inputWorld.value !=='') ? true: false;
+    let bandInput1 = (input1.value !=='') ? true: false;
+    let bandInput2 = (input2.value !=='') ? true: false;
+    let bandInput3 = (input3.value !=='') ? true: false;
+
+    if( bandInput1 && bandInputWorld && bandInput2 && bandInput3){
+        debugger;
+        return true;
+    }
+    else{
+        debugger;
+        return false;
+    }
+}

9ab7841ee940301b56f439ba9de991bc51edbe0b
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2022-01-16 16:03:53 -0600

    check letters

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,6 +3,7 @@ import DrawingPerson from "./DrawingPerson.js";
 
 let world;
 
+
 let alphabet = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let NumbersAndSymbols = ['0','1','2','3','4','5','6','7','8','9','0','_'];
 
@@ -33,22 +34,65 @@ const divButtons3 = document.getElementById("buttons3");
 
 function samplehandler(event){
     const element=  event.target; 
-    debugger;
     alert(`Onclick: "${element.innerText}"`);
+    const bandFound = checkLetter(element.innerText);
+    if(bandFound){
+        wrotteWorld();
+    }
     element.style.display='none';
     element.style.visibility = 'hidden';
+
+}
+
+
+function wrotteWorld() {
+    const infoP = document.getElementById(`Information`);
+    const worldIncognit = world.map(
+        function (element) {
+            if(element.idFound==false){
+                return " _ ";
+            }
+            else{
+                return `${element.letter}`;
+            }
+        }
+    );
+    infoP.innerText= worldIncognit.toString();
+}
+
+function checkLetter(Letter) {
+    let value = world.find(element=> element.letter.toUpperCase()== Letter.toUpperCase());    
+    debugger;
+    if( value !== undefined){
+        ///Letra encontrada
+        alert(`La letra ${letter.innerText} ha sido encontrada`);
+        for (let I = 0; I < world.length; I++) {
+            if(world[I].letter == Letter){
+                world[I].idFound= true;            
+            }            
+        };
+        debugger;
+        return true;
+    }
+    else{
+        ///Letra No encontrada
+        alert(`Letra no encontrada`);
+        debugger;
+        return false;
+    }    
 }
 
 
 function initPlay(){
     let general =new  GeneralMethods();
+
     const IsValid = general.validateValues([inputWorld.value,input1.value,input2.value,input3.value]);
 
     if(IsValid){
         errorMessage.innerText= "";
         div1.style.display= "none";
         const listofBtns= general.GenerateLetters([alphabet,divButtons2]);
-        const listOfNumbers = general.GenerateLetters([NumbersAndSymbols,divButtons3]);
+        const listOfNumbers = general.GenerateLetters([NumbersAndSymbols,divButtons3]);        
         listofBtns.forEach(element => {
             element.addEventListener('click',samplehandler);
             divButtons2.appendChild(element);
@@ -57,8 +101,19 @@ function initPlay(){
             element.addEventListener('click',samplehandler);
             divButtons2.appendChild(element);
         });
-        div2.style.display="inherit";        
+        div2.style.display="inherit"; 
+
+        const tmpworld = Array.from(inputWorld.value);
+        world = tmpworld.map(function (element) {
+            let tmpObjet = {
+                letter : element.toUpperCase(),
+                idFound: false
+            };
+            return tmpObjet;
+        });
+        wrotteWorld();    
     }
+
     else{
         errorMessage.innerText= "¡No introdujo los datos necesarios!";
     }
@@ -71,6 +126,8 @@ pase.addEventListener('click',()=>{
     div1.style.display= "none";
     div2.style.display="inherit";  
 });
+
+
 
 
 

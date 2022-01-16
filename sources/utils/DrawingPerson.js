4d80d60952b1bd1711c9d181b9fef56295da1019
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2022-01-16 17:56:40 -0600

    pruebas en canvas

--- sources/utils/DrawingPerson.js
+++ sources/utils/DrawingPerson.js
@@ -3,32 +3,6 @@ class drawingPerson{
     canvas= document.getElementById("canvas")
     ctx = this.canvas.getContext("2d");
 
-    cesped=() =>{
-        ctx.fillStyle = "green";
-        ctx.fillRect(0,430 , 499,70);
-    }
-    poste=() =>{
-        ctx.fillStyle = "brown";
-        ctx.fillRect(120,80 , 30,350);
-    }
-    posteHorizontal=() =>{
-        ctx.fillStyle = "brown";
-        ctx.fillRect(120,80 , 190,15);
-    }
-    posteVerticalPequeño=() =>{
-        ctx.fillStyle = "brown";
-        ctx.fillRect(302, 80 , 8,80); 
-    }
-    cabeza=() =>{
-        ctx.beginPath();	  
-        ctx.arc(304,195,35, 0, 2*Math.PI);
-        ctx.lineWidth = 3;
-        ctx.stroke();
-    }
-    torso=() =>{
-        fillStyle = "back";
-        fillRect(284,228 ,40 ,90)
-    }
 }
 
 

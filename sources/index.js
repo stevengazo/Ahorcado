2847307dbb3cb4e4dae9cf5a72d8823cf1e1b112
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2022-02-06 18:44:24 -0600

    documentation of the code

--- sources/index.js
+++ sources/index.js
@@ -1,18 +1,36 @@
+/// Imports of the document
 import GeneralMethods from "./utils/GeneralMethods.js";
 import DrawingPerson from "./utils/DrawingPerson.js";
 
-let draw = new DrawingPerson();
-
+// variables of the elements in the document
+const div1 = document.getElementById("Part1");
+const div2 = document.getElementById("Part2");
+const errorMessage = document.getElementById("message");
+// Input text fields of the document
+const inputWorld = document.getElementById("world");
+const prueba1 = document.getElementById("prueba1");
+const prueba2 = document.getElementById("prueba2");
+const prueba3 = document.getElementById("prueba3");
+// helps for the playes
+const divButtons2 = document.getElementById("buttons2");
+const divButtons3 = document.getElementById("buttons3");
+// variable to save the word;
 let world;
-
-
+// alphabet and numbers available to use
 let alphabet = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let NumbersAndSymbols = ['0','1','2','3','4','5','6','7','8','9','0','-'];
+// label of the attemps
+const lblAttemps = document.getElementById("lblIntentos");
 
+let draw = new DrawingPerson();
+// Quantity of attends by word 
+let quantityOfAttemps = 10;
 
+/// Button Play - Interface 1
 const buttonPlay= document.getElementById("buttonPlay");
 buttonPlay.addEventListener("click",initPlay);
 
+// Button to clean the input text fields
 const btnClean = document.getElementById("btnClean");
 btnClean.addEventListener('click',()=>{
     const general = new GeneralMethods();
@@ -20,32 +38,23 @@ btnClean.addEventListener('click',()=>{
 });
 
 
-const div1 = document.getElementById("Part1");
-const div2 = document.getElementById("Part2");
 
-const errorMessage = document.getElementById("message");
-
-const inputWorld = document.getElementById("world");
-const prueba1 = document.getElementById("prueba1");
-const prueba2 = document.getElementById("prueba2");
-const prueba3 = document.getElementById("prueba3");
-
-const divButtons2 = document.getElementById("buttons2");
-const divButtons3 = document.getElementById("buttons3");
-
-
+/// Function to add in the addEventListener of every button in the second step (alphabet)
+/// Received how parameter the letter to check
 function samplehandler(event){
     const element=  event.target; 
     const bandFound = checkLetter(element.innerText);
     if(bandFound){
         wrotteWorld();
+    }else{
+        quantityOfAttemps --;
+        lblAttemps= ``;
     }
     element.style.display='none';
     element.style.visibility = 'hidden';
-
 }
 
-
+/// Generate the positions and render the letter in the page
 function wrotteWorld() {
     const infoP = document.getElementById(`Information`);
     const worldIncognit = world.map(
@@ -61,11 +70,13 @@ function wrotteWorld() {
     infoP.innerText= worldIncognit.toString();
 }
 
+
+/// Check if the letter is correct or incorrect
 function checkLetter(Letter) {
     let value = world.find(element=> element.letter.toUpperCase()== Letter.toUpperCase());    
     debugger;
     if( value !== undefined){
-        ///Letra encontrada
+        /// letter found
         const info_Message= document.getElementById("info-Message");
         info_Message.style.color= "green";
         info_Message.innerText=`${Letter} se encontró`;
@@ -77,7 +88,7 @@ function checkLetter(Letter) {
         return true;
     }
     else{
-        ///Letra No encontrada
+        /// not found 
         const info_Message= document.getElementById("info-Message");
         info_Message.style.color= "red";
         info_Message.innerText=`${Letter} no se encontró`;
@@ -86,17 +97,16 @@ function checkLetter(Letter) {
 }
 
 
-function initPlay(){
-    let general =new  GeneralMethods();
 
+/// inicializate the game
+function initPlay(){
+    // check the letter and the others inputs
+    let general =new  GeneralMethods();
     const IsValid = general.validateValues([inputWorld.value,prueba1.value,prueba2.value,prueba3.value]);
     const checkLetters = general.foundInvalidLetters(inputWorld.value,alphabet,NumbersAndSymbols);
     const invalidLetters = (checkLetters.length !== 0) ? true : false;
 
-
-
-
-
+    /// Return error message or continue with the game
     if(invalidLetters)
     {
         errorMessage.innerText= `Letras no validas ${checkLetters}`;
@@ -105,9 +115,11 @@ function initPlay(){
         errorMessage.innerText= "¡No introdujo los datos necesarios!";
     }
     else
-    {        
+    {    
+        lblAttemps.innerText= `Quedan ${quantityOfAttemps} Intentos`;    
         errorMessage.innerText= "";
         div1.style.display= "none";
+        /// Generate the letters and numbers buttons 
         const listofBtns= general.GenerateLetters([alphabet,divButtons2]);
         const listOfNumbers = general.GenerateLetters([NumbersAndSymbols,divButtons3]);        
         listofBtns.forEach(element => {
@@ -118,8 +130,10 @@ function initPlay(){
             element.addEventListener('click',samplehandler);
             divButtons2.appendChild(element);
         });
+        // display the second "interface"
         div2.style.display="inherit"; 
 
+        /// Generate an array from the input word and save the array in the variable "word"
         const tmpworld = Array.from(inputWorld.value);
         world = tmpworld.map(function (element) {
             if(element===" "){
@@ -137,14 +151,12 @@ function initPlay(){
 }
 
 
-
+// Display the clues
 const pase = document.getElementById('pase');
 pase.addEventListener('click',()=>{
     div1.style.display= "none";
     div2.style.display="inherit";  
 });
-
-
 const pista1 = document.getElementById('pista-1');
 pista1.addEventListener('click',(element)=>{        
     pista1.innerText = prueba1.value;
